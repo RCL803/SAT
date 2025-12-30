0ce54b6bac5a0265725cf3a96e977e5a6328a0a0
RCL803 <ns853851@yahoo.com.tw>
2025-12-31 03:55:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -95,14 +95,7 @@ fetch('https://api.github.com/repos/RCL803/SAT/commits?path=images&per_page=1')
   })
   .catch(error => console.error('Error fetching latest image:', error));
 
-fetch('https://raw.githubusercontent.com/RCL803/SAT/main/bp.json?t=' + Date.now())
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("SYS").textContent = "測試成功: " + (data.SYS || "沒抓到資料");
-    document.getElementById("DIA").textContent = data.DIA;
-    document.getElementById("result").textContent = data.result;
-  })
-  .catch(err => console.error('讀取血壓數據失敗:', err));
+
 </script>
 
 
